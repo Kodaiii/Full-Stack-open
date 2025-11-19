34a48940ca76cc589b51a2eb8825a6fb3a72c4f5
Kodaiii <adamamripn@gmail.com>
2025-11-19 22:56:13 +0100

    Exercises 1.6 to 1.8

---
+++ Part1/Unicafe/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+
+const Button = ({onClick, text}) => {
+    return (
+        <button onClick={onClick}> {text} </button>
+    )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+    const all = good + neutral + bad
+    const average = (good - bad / all)
+    const positive = (good / all) * 100 + " %"
+    return (
+        <div>
+            <h1>statistics</h1>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {average}</p>
+            <p>positive {positive}</p>
+        </div>
+    )
+}
+
+
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGoodClick = () => {
+        setGood(good + 1)
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1)
+    }
+
+    return (
+        <div>
+            <h1>give feedback</h1>
+            <Button onClick={handleGoodClick} text="good"/>
+            <Button onClick={handleNeutralClick} text="neutral"/>
+            <Button onClick={handleBadClick} text="bad"/>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
+        </div>
+    )
+}
+
+export default App
