ff4a20244545457bf83bd0f34e4ba3ffdd312cd9
Kodaiii <adamamripn@gmail.com>
2025-11-20 15:31:26 +0100

    Exercises 1.9 to 1.11

--- Part1/Unicafe/src/App.jsx
+++ Part1/Unicafe/src/App.jsx
@@ -11,17 +11,48 @@ const Statistics = ({good, neutral, bad}) => {
     const all = good + neutral + bad
     const average = (good - bad / all)
     const positive = (good / all) * 100 + " %"
-    return (
-        <div>
-            <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {average}</p>
-            <p>positive {positive}</p>
-        </div>
-    )
+    if (all === 0) {
+        return (
+            <div>
+                <h1>statistics</h1>
+                <p>No feedback given</p>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>statistics</h1>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>good</td>
+                        <td>{good}</td>
+                    </tr>
+                    <tr>
+                        <td>neutral</td>
+                        <td>{neutral}</td>
+                    </tr>
+                    <tr>
+                        <td>bad</td>
+                        <td>{bad}</td>
+                    </tr>
+                    <tr>
+                        <td>all</td>
+                        <td>{all}</td>
+                    </tr>
+                    <tr>
+                        <td>average</td>
+                        <td>{average}</td>
+                    </tr>
+                    <tr>
+                        <td>positive</td>
+                        <td>{positive}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
 
 
